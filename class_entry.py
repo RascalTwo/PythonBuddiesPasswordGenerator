26e54ba85ff31668a8cf4036ae6dd8421c3f6347
Rascal_Two <therealrascaltwo@gmail.com>
2020-04-15 15:31:06 -0400

    Refactor Custom Entry input
    Move arguments into initializer signature
    Seperate placeholder text logic from style
    
    Close #5

--- class_entry.py
+++ class_entry.py
@@ -1,31 +1,50 @@
+"""Custom entry fields capable of containing placeholder text"""
+
 import tkinter as tk
 
 class Entry(tk.Entry):
-	def __init__(self, root = None, *args, **kwargs):
-		self.alt_text = kwargs.pop('alt_text', None)
-		self.fg = kwargs.pop('fg', '#505050')
-		self.active_fg = kwargs.pop('active_fg', '#181818')
-		self.bg = kwargs.pop('bg', '#a8a8a8')
+	"""Custom entry field with placeholder text"""
+	def __init__(self, *args, alt_text=None, alt_fg='#505050', fg='#181818', bg='#a8a8a8', font=('Arial', 18, 'bold'), borderwidth=2, relief=tk.SOLID, **kwargs):
+		self.alt_text = alt_text
+		self.alt_fg = alt_fg
+		self._fg = fg
 
-		super().__init__(master = root, font=kwargs.pop('font', ('Arial', 18, 'bold')), bg=self.bg, fg=self.fg, borderwidth=kwargs.pop('borderwidth', 2), relief=kwargs.pop('relief', 'solid'))
-		
-		self.bind('<FocusIn>', self.on_focus)
-		self.bind('<FocusOut>', self.on_defocus)
+		super().__init__(
+			bg=bg,
+			fg=self.alt_fg if self.alt_text else self._fg,
+			font=font,
+			borderwidth=borderwidth,
+			relief=relief,
+			*args,
+			**kwargs
+		)
+
 
 		if self.alt_text:
 			self.insert(tk.END, self.alt_text)
+			self.bind('<FocusIn>', self.on_focus_in)
+			self.bind('<FocusOut>', self.on_focus_out)
 
-	def on_focus(self, _):
-		if self.alt_text and self.get().strip() != self.alt_text and self.cget('fg') != self.fg:
+		self.has_user_content = False
+
+	def on_focus_in(self, _):
+		"""Remove placeholder text if exists"""
+		# If text equals alt text, and there is no user content, delete alt text
+		if self.get().strip() != self.alt_text or self.has_user_content:
 			return
 
-		self.config(fg = self.active_fg)
+		self.configure(fg=self._fg)
 		self.delete(0, tk.END)
+		self.has_user_content = True
 
-	def on_defocus(self, _):
-		if not self.alt_text or self.get().strip():
+
+	def on_focus_out(self, _):
+		"""Add placeholder text if no user content"""
+		# If no text exists, and there is user content, insert alt text
+		if self.get().strip() or not self.has_user_content:
 			return
 
 		self.delete(0, tk.END)
-		self.config(fg = self.fg)
+		self.configure(fg=self.alt_fg)
 		self.insert(tk.END, self.alt_text)
+		self.has_user_content = False
